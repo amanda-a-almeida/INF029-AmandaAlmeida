2c3ff1953f8a1962a96ea1da0b748df94b63bcfd
Amanda Almeida <93677553+amanda-a-almeida@users.noreply.github.com>
2022-03-16 18:27:14 -0300

    Update cadastroPessoasV0.c

--- ProjetoEscolaV0/cadastroPessoasV0.c
+++ ProjetoEscolaV0/cadastroPessoasV0.c
@@ -1,17 +1,1 @@
-#ifndef cadastroPessoasV0_H
-#define cadastroPessoasV0_H
-
-struct dados_pessoa
-{
-    int matricula;
-    char nome[50];
-    char sexo; // M , F
-    int dia_nascimento;
-    int mes_nascimento;
-    int ano_nascimento; 
-    char cpf[15];
-    int classificacao; // 0 - professor / 1 - aluno
-
-}
-
-#endif /* adastroPessoasV0_H */
+/**/
