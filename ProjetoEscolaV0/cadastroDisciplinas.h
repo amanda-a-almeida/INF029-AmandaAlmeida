196787506e9f408e086c2aaea6ba3f83ef949345
Amanda Almeida <93677553+amanda-a-almeida@users.noreply.github.com>
2022-03-16 18:28:22 -0300

    Update cadastroDisciplinas.h

---
+++ ProjetoEscolaV0/cadastroDisciplinas.h
@@ -0,0 +1,9 @@
+/*  Struct de cadatro da Disciplinas   */
+struct cadastro_disciplina
+{
+    char nome_disciplina[50];
+    char codigo_disciplina[6];
+    int semestre;
+    char matricula_professor[50];
+
+};
