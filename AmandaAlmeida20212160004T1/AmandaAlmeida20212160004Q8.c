eea380a3ce1b93ad795d6398a01fcc4d98c117cf
Amanda Almeida <93677553+amanda-a-almeida@users.noreply.github.com>
2022-04-27 22:17:28 -0300

    Create AmandaAlmeida20212160004Q8.c

---
+++ AmandaAlmeida20212160004T1/AmandaAlmeida20212160004Q8.c
@@ -0,0 +1,25 @@
+// #################################################
+//  Instituto Federal da Bahia
+//  Salvador - BA
+//  Curso de Análise e Desenvolvimento de Sistemas http://ads.ifba.edu.br
+//  Disciplina: INF029 - Laboratório de Programação
+//  Professor: Renato Novais - renato@ifba.edu.br
+
+//  ----- Orientações gerais -----
+//  Descrição: esse arquivo deve conter as questões do trabalho do aluno.
+//  Cada aluno deve renomear esse arquivo para Aluno<MATRICULA>.c
+//  O aluno deve preencher seus dados abaixo, e implementar as questões do trabalho
+
+//  ----- Dados do Aluno -----
+//  Nome: Amanda Araújo Almeida
+//  email: amanda.mecatronica@gmail.com
+//  Matrícula: 20212160004
+//  Semestre: 2
+
+//  Copyright © 2016 Renato Novais. All rights reserved.
+// Última atualização: 07/05/2021
+
+// #################################################
+
+// Questão 08
+
